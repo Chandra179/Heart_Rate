5dd1eca21118680339d7c51e3384e7d96f2c1a24
Chandra <chandrafirst67@gmail.com>
2020-10-23 12:15:17 +0700

    graph

--- HeartRate/lib/pages/home.dart
+++ HeartRate/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:heart_rate/component/barChart.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -37,23 +38,54 @@ class _HomeState extends State<Home> {
           ],
         ),
       ),
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                children: [
-                  Text('Chandra')
-                ],
+      body: Container(
+        child: ListView(children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(50, 50, 50, 0),
+            alignment: Alignment.center,
+            child: IconButton(
+              icon: Icon(
+                Icons.favorite,
               ),
-              Column(
-                children: [
-                  Text('Chandra')
+              iconSize: 150,
+              color: Colors.pink,
+              splashRadius: 1,
+              onPressed: () {},
+            ),
+          ),
+          Center(child: Text('Click Me')),
+          Container(
+            alignment: Alignment.topLeft,
+            padding: EdgeInsets.fromLTRB(100, 70, 70, 10),
+            child: Text('Recently'),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(95, 0, 70, 10),
+            child: Card(
+              child: Column(
+                children: <Widget>[
+                  const ListTile(
+                    leading: Icon(Icons.access_alarms),
+                    title: Text('Exercise'),
+                    subtitle: Text('Cycling through deep jungle in midnight'),
+                  )
                 ],
               )
-            ]
-        ),
-      )
+            ),
+          ),
+          Container(
+            alignment: Alignment.topLeft,
+            padding: EdgeInsets.fromLTRB(100, 40, 70, 10),
+            child: Text('Heart Monitor'),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(95, 10, 70, 10),
+            child: BarChartSample3(),
+          )
+
+        ],),
+      ),
+
     );
   }
 }
